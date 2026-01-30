718fe2c00e54d5581069d132cddbcbc5dd1a55f0
sophhieshen <sophieshen06@gmail.com>
2026-01-30 02:14:18 -0800

    Firebase Added

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA8b1n6HRIY3pumwvisJKc0bHU0K3r9xh0",
+  authDomain: "lavalab-s26.firebaseapp.com",
+  projectId: "lavalab-s26",
+  storageBucket: "lavalab-s26.firebasestorage.app",
+  messagingSenderId: "492263001064",
+  appId: "1:492263001064:web:e5fe03d2b0963358e2b25d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
