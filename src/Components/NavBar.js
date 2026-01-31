e468f205d082fcfec51d4e6b8103abcb58e3bda3
sophhieshen <sophieshen06@gmail.com>
2026-01-30 19:08:58 -0800

    Updated Navigation Bar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import './NavBar.css';
 
-import logo from '../assets/images/logo512.png';
+import logo from '../assets/images/logo.svg';
+import cart from '../assets/images/cart.svg';
+import profile from '../assets/images/profile.svg';
+import heart from '../assets/images/heartEmpty.svg';
 
 const NavBar = () => {
   return (
     <nav className="navBar">
-      <img src={logo} alt="Logo" className="logo" />
+      <div className="navBarInner">
+        <img src={logo} alt="Logo" className="logo" />
 
-      <ul className="navLinks">
-        <li>Women</li>
-        <li>Men</li>
-        <li>Kids</li>
-        <li>Classic</li>
-        <li>Sport</li>
-        <li>Sale</li>
-      </ul>
+        <ul className="navLinks">
+          <li>Women</li>
+          <li>Men</li>
+          <li>Kids</li>
+          <li>Classic</li>
+          <li>Sport</li>
+          <li>Sale</li>
+        </ul>
+
+        <div className="navIcons">
+          <img src={heart} alt="Wishlist" className="navIcon" />
+          <img src={cart} alt="Cart" className="navIcon" />
+          <img src={profile} alt="Profile" className="navIcon" />
+        </div>
+      </div>
     </nav>
   );
 };
